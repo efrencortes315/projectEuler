f8bc3e0f895620fb12f8ecec10bcf2d9efa1ac85
efrencortes315 <efren.cortes315@gmail.com>
2017-10-30 12:46:16 -0400

    new algorithm for checking palindrome, UNSOLVED

--- p4.cpp
+++ p4.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 #include <limits.h>
+#include <string>
+#include <algorithm>
 using namespace std;
 bool isPalindrome(int);
 bool isPalindrome(int num){
-        unsigned int number_of_digits = 0;
+	string sNum=std::to_string(num);
+	string rNum= reverse(sNum.begin(),sNum.end());
+	if(sNum == rNum){
+		return true;
+	}else{
+		return false;	
+	}
+       /* unsigned int number_of_digits = 0;
         int n = num;
         do {
         ++number_of_digits;
@@ -16,30 +25,28 @@ bool isPalindrome(int num){
                 size[i]= n%10;
                 n/=10;
         }
-        cout << "num digs:" << number_of_digits << endl;
-        cout << "num:" << num << endl;
-        for(int i=0;i<number_of_digits/2;i++){
+        for(int i=0;i<=number_of_digits/2;i++){
                 if(size[i]!=size[number_of_digits-i-1]){
                         return false;
                 }
                 return true;
         }
-        cout << endl;
+        cout << endl;*/
 }
 
 int main(){
-	
+	cout << "calculating..." << endl;
+	int largest=0;
 	for(int i=999;i>0;i--){
 		for(int j=999;j>0;j--){
 			int prod = i*j;
-			if(isPalindrome(prod)){
-				cout << prod << endl;
-				break;
+			if(isPalindrome(prod)&& prod > largest){
+				largest=prod;
+						
 			}
 		}	
 	}
-	
-	
+	cout << "the answer is: " << largest << endl;
 	return 0;
 }
 
