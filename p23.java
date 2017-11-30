13c0fc42c7ed74f7cc866fbd6feea2f16a50a563
efrencortes315 <efren.cortes315@gmail.com>
2017-11-30 02:47:36 -0500

    p23, unsolved

--- p23.java
+++ p23.java
@@ -1,9 +1,22 @@
 import java.util.*;
 import java.util.ArrayList;
+import java.math.BigInteger;
 class p23{
 public static void main(String[] args){
 ArrayList<Integer> myList = makeList();
-
+Set<Integer> sumsOfTwoAbund = new HashSet<Integer>();
+for(int i=0;i<myList.size();i++){
+	for(int j=0;j<myList.size();j++){
+		sumsOfTwoAbund.add(myList.get(i)+myList.get(j));		
+	}
+}
+BigInteger sum = new BigInteger("0");
+for(int i=1;i<=28123;i++){
+	if(!sumsOfTwoAbund.contains(i)){
+		sum = sum.add(new BigInteger(Integer.toString(i)));	
+	}
+}
+System.out.println(sum);
 }
 public static ArrayList<Integer> makeList(){
 ArrayList<Integer> tmp = new ArrayList<Integer>();
@@ -19,7 +32,6 @@ public static  int sumOfDivisors(int num){
 	int sum=1;
 	for(int i=2;i<Math.sqrt(num);i++){
 		if(num%i==0){
-		System.out.println(sum + " " + num/i);
 		sum+=(i+((int)num/i));
 		}
 	}
